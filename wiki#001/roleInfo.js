82a3610144be3a68b8ab992b6b35bbab24304f83
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2023-09-09 12:18:51 +0530

    Update roleInfo.js

--- wiki#001/roleInfo.js
+++ wiki#001/roleInfo.js
@@ -1,1 +1,20 @@
+module.exports = [{
+    name: "role",
+    type: "interaction",
+    prototype: "slash",
+    code: `
 
+$interactionReply[;{newEmbed:{title:Role info}{description:**Name#COLON#** $roleName[$get[role]] (<@&$get[role]>)\n**Color#COLON#** $get[color]\n**Members Count#COLON#** $roleMembersCount[$get[role]]\n**Created on#COLON#** <t#COLON#$get[date]#COLON#f>\n**Position#COLON#** $rolePosition[$get[role]]\n**Hoisted?** $toLocaleUpperCase[$isHoisted[$get[role]]]\n**Editable?** $toLocaleUpperCase[$isRoleEditable[$get[role]]]}{footer:ID#COLON# $get[role]}{color:$get[color]}}]
+
+$let[date;$djsEval[
+Math.floor(message.guild.roles.cache.get('$get[role]').createdAt.getTime() /1000);true]]
+
+$let[color;$getRoleColor[$get[role]]]
+
+$let[role;$slashOption[role]]
+
+$suppressErrors[Something went wrong!{options:{ephemeral:true}}{extraOptions:{interaction:true}}]
+
+$onlyIf[$interactionData[options._subcommand]==info;]
+`
+}]
