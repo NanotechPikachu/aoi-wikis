ef13a243d4dd9b8a1e12b27863d994b4d711c1d6
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2023-09-18 07:02:55 +0530

    Update roleMassAdd.js

--- wiki#001/roleMassAdd.js
+++ wiki#001/roleMassAdd.js
@@ -1,3 +1,67 @@
-/*
-In development. Soon...
-*/
+module.exports = [{
+    name: "role",
+    type: "interaction",
+    prototype: "slash",
+    code: `
+$djsEval[(async () => {
+const { EmbedBuilder } = require('discord.js');
+const rol = "$get[role]";
+const guild = message.guild;
+const role = guild.roles.cache.get(rol);
+const int = d.data.interaction
+const userHigh = int.member.roles.highest;
+
+let suc = 0;
+let er = 0;
+
+const emb = new EmbedBuilder()
+      .setDescription("The process of role adding has been started. Please wait...")
+      .setColor(0x008000)
+
+await int.reply({ embeds: [emb] })
+const start = Date.now()
+
+const reason = "Role added by "+int.user.username 
+
+for (const member of guild.members.cache) {
+    const memHigh = member[1].roles.highest;
+    if (userHigh.comparePositionTo(memHigh)>0&&member[1].manageable) {
+        try {
+            member[1].roles.add(role, reason)
+            suc+=1;
+        } catch (err) {
+            console.error(err);
+            er+=1;
+        }
+    } else {
+        er+=1;
+    } 
+    
+}
+const embed = new EmbedBuilder()
+      .setDescription(\`**Process Complete**\n\n<@&\${role.id}> has been successfully added to \${suc} and failed on \${er} members\`)
+      .setColor(0x008000)
+      .setFooter({ text: \`Time taken: Around \${Math.floor((Date.now()-start)/1000)} seconds.\` })
+await int.editReply({ embeds: [embed] })
+})()
+]
+
+$onlyIf[$checkContains[$rolePerms[$get[role]; , ;$guildID];administrator]==false;{newEmbed:{description:You cannot do this operation on a role which has \`ADMINISTRATOR\` permission.}{color:Red}}{options:{ephemeral:true}}{extraOptions:{interaction:true}}]
+
+$onlyIf[$isRoleManaged[$get[role]]==false;{newEmbed:{description:You cannot do this operation on a managed role.}{color:Red}}{options:{ephemeral:true}}{extraOptions:{interaction:true}}]
+
+$onlyIf[$rolePosition[$userHighestRole[$authorID]]<$rolePosition[$get[role]];{newEmbed:{description:You cannot do the operation on a higher role than you.}{color:Red}}{options:{ephemeral:true}}{extraOptions:{interaction:true}}]
+
+$onlyIf[$rolePosition[$userHighestRole[$clientID]]<$rolePosition[$get[role]];{newEmbed:{description:I cannot do the operation on a higher role than me.}{color:Red}}{options:{ephemeral:true}}{extraOptions:{interaction:true}}]
+
+$onlyClientPerms[manageroles;{newEmbed:{description:I do not have \`MANAGE_ROLES\` permission.}{color:Red}}{options:{ephemeral:true}}{extraOptions:{interaction:true}}]
+
+$onlyPerms[administrator;{newEmbed:{description:You do not have \`ADMINISTRATOR\` permission.}{color:Red}}{options:{ephemeral:true}}{extraOptions:{interaction:true}}]
+
+$let[role;$slashOption[role]]
+
+$suppressErrors[Something went wrong!{options:{ephemeral:true}}{extraOptions:{interaction:true}}]
+    
+$onlyIf[$interactionData[options._subcommand]==mass-add;]
+`
+}]
