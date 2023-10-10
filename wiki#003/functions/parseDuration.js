a999ba950bc55999c112f9c27e10134026e375f7
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2023-10-10 11:03:55 +0530

    Made parseDuration.js

--- wiki#003/functions/parseDuration.js
+++ wiki#003/functions/parseDuration.js
@@ -1,1 +1,28 @@
+function parseDuration(duration) {
+  const regex = /(\d+)([smhdw])/;
+  const match = duration.match(regex);
+  if (!match) return null;
+
+  const value = parseInt(match[1]);
+  const unit = match[2];
+
+  switch (unit) {
+    case 's':
+      return value * 1000; // seconds
+    case 'm':
+      return value * 60 * 1000; // minutes
+    case 'h':
+      return value * 60 * 60 * 1000; // hours
+    case 'd':
+      return value * 24 * 60 * 60 * 1000; // days
+    case 'w':
+      return value * 7 * 24 * 60 * 60 * 1000; //weeks
+    default:
+      return null;
+  }
+}
+
+module.exports = {
+    parseDuration
+}
 
