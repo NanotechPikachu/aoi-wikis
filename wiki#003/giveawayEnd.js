b6fbb7c7da4d8f2f3a301c81300ccc52d4ce0e83
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2023-10-10 11:01:36 +0530

    Coded giveawayEnd.js

--- wiki#003/giveawayEnd.js
+++ wiki#003/giveawayEnd.js
@@ -1,1 +1,18 @@
 
+module.exports = [{
+    name: "giveaway end",
+    aliases: ["gw-end", "gend"],
+    code: `
+$djsEval[(async () => {
+const { stopGiveaway } = require('functions/giveawayManager.js');
+const giveawayId = "$message[1]";
+
+stopGiveaway(giveawayId, message);
+})();
+]
+
+$onlyIf[$isNumber[$message[1]]==true;{newEmbed:{description:Enter a valid message ID of giveaway}{color:Red}}]
+$onlyPerms[manageguild;{newEmbed:{description:You do not have \`MANAGE_GUILD\` permission.}{color:Red}}]
+$suppressErrors[Something went wrong ]
+`
+}]
