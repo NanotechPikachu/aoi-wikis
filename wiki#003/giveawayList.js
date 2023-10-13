5371b6680f55c178a9d95bba0268ae161e0fbd61
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2023-10-13 13:42:52 +0530

    Make giveawayList.js cmd

--- wiki#003/giveawayList.js
+++ wiki#003/giveawayList.js
@@ -1,1 +1,12 @@
-
+module.exports = [{ 
+     name: "giveaway list", 
+     aliases: ["glist", "gw-list"], 
+     code: ` 
+ $djsEval[(async () => { 
+ const { giveawaysList } = require('functions/giveawayManager.js'); 
+ giveawaysList(message); 
+ })(); 
+ ] 
+ $onlyPerms[manageguild;{newEmbed:{description:You do not have \`MANAGE_GUILD\` permission.}{color:Red}}] 
+ ` 
+ }]
