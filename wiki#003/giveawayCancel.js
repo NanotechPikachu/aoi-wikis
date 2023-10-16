7de02ae59ac695d6e106d057472ccaffcec584d7
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2023-10-16 13:45:04 +0530

    New giveawayCancel.js

--- wiki#003/giveawayCancel.js
+++ wiki#003/giveawayCancel.js
@@ -1,1 +1,19 @@
+module.exports = {
+    name: "giveaway cancel",
+    aliases: ["gcancel", "gw-cancel"],
+    code: `
+$djsEval[(async () => {
+const { cancelGiveaway } = require('functions/giveawayManager.js');
 
+const giveawayId = "$message[1]";
+const reason = "$messageSlice[1]";
+
+cancelGiveaway(giveawayId, reason, message);
+})();
+]
+$onlyIf[$messageSlice[1]!=;{newEmbed:{description:Please enter a valid reason for cancellation of Giveaway.}{color:Red}}]
+$onlyIf[$isNumber[$message[1]]==true;{newEmbed:{description:Please enter a valid message ID}{color:Red}}]
+$onlyPerms[manageguild;{newEmbed:{description:You do not have \`MANAGE_GUILD\` permission.}{color:Red}}]
+$suppressErrors[Something went wrong!]
+`
+}
