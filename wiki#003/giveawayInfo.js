56b9f8fd6c74f8e04739b86fe1bfa5841594b34b
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2023-10-22 18:22:37 +0530

    Make of giveawayInfo.js

--- wiki#003/giveawayInfo.js
+++ wiki#003/giveawayInfo.js
@@ -1,3 +1,18 @@
-/*
-On developmen
-*/
+module.exports = {
+    name: "giveaway info",
+    aliases: ["ginfo", "gw-info"],
+    code: `
+$djsEval[(async () => {
+const { giveawayInfo } = require('/home/container/functionsJS/giveawayManager.js');
+
+const giveawayId = "$message[1]";
+
+giveawayInfo(giveawayId, message);
+})();
+]
+
+$onlyIf[$isNumber[$message[1]]==true;{newEmbed:{description:Enter a valid message ID of giveaway}{color:Red}}]
+$onlyPerms[manageguild;{newEmbed:{description:You do not have \`MANAGE_GUILD\` permission.}{color:Red}}]
+$suppressErrors[Something went wrong ]
+`
+}
