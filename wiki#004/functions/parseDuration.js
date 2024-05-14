b0d68698fe9971bf7388775903bde7e414f44b25
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-05-14 18:00:51 +0530

    Make parseDuration function

---
+++ wiki#004/functions/parseDuration.js
@@ -0,0 +1,35 @@
+/*
+  FUNCTION: parseDuration()
+  USE: To convert the time in short format to ms which will be acknowledged by node
+  PARAMS: 
+    'duration': | Type: 'String' |
+*/
+
+
+function parseDuration(duration) {
+  const regex = /(\d+)([smhdw])/;
+  const match = duration.match(regex);
+  if (!match) return null;
+
+  const value = parseInt(match[1]);
+  const unit = match[2];
+
+  switch (unit) {
+    case 's':
+      return value * 1000; // seconds
+    case 'm':
+      return value * 60 * 1000; // minutes
+    case 'h':
+      return value * 60 * 60 * 1000; // hours
+    case 'd':
+      return value * 24 * 60 * 60 * 1000; // days
+    case 'w':
+      return value * 7 * 24 * 60 * 60 * 1000; //weeks
+    default:
+      return null;
+  };
+};
+
+module.exports = {
+    parseDuration
+};
