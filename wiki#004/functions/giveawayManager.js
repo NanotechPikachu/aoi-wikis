7dc8785b48f09309beff6c7ce23281ccf2a93fd2
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-05-15 06:38:01 +0530

    Edit: start giveaway func

--- wiki#004/functions/giveawayManager.js
+++ wiki#004/functions/giveawayManager.js
@@ -17,3 +17,51 @@ const {
     ButtonBuilder, 
     ButtonStyle 
       } = require('discord.js');
+
+/*
+  FUNCTION: startGiveaway()
+  USE: To start a giveaway and send it's embed.
+  PARAMS:
+    'prize': | Type: 'String' |
+    'duration': | Type: 'String' |
+    'interaction': | Type: 'Object' | 
+    'winnersCount': | Type: 'Number' |
+    'host': | Type: 'String' |
+    'channelId': | Type: 'String' |
+*/
+
+async function startGiveaway(prize, duration, interaction, winnersCount, host, channelId) {
+  const em = new EmbedBuilder()
+        .setTitle('Giveaway!')
+        .setDescription(`**Giveaway Prize:** ${prize}\n**Winner(s):** ${winnersCount}\n**Host:** ${host}\n**Ends on:** <t:${Math.floor((Date.now() + parseDuration(duration))/1000)}:f> (<t:${Math.floor((Date.now() + parseDuration(duration))/1000)}:R>)\n\nClick to join!`)
+        .setColor(0xFFA500);
+  try {
+  const channel = await interaction.guild.channels.cache.get(channelId);
+  const row = new ActionRowBuilder()
+  	.addComponents(
+    	new ButtonBuilder()
+        	.setLabel('🎉 0')
+        	.setCustomId('join-gw')
+        	.setStyle(ButtonStyle.Primary)
+    );
+  const giveawayMessage = await channel.send({ embeds: [em], components: [row] });
+    
+  const collector = giveawayMessage.createMessageComponentCollector({
+      componentType: ComponentType.Button
+  });
+    
+  collector.on("collect", async (i) => {
+      await i.deferReply({ ephemeral: true });
+      const p = await participant(i);
+      await i.followUp(`You have ${p ? "entered" : "exited"} the giveaway!`);
+    });  
+
+  const endTime = Date.now() + parseDuration(duration);
+  const guildId = interaction.guild.id;
+  const hoster = host;
+
+  await addGiveaway(giveawayMessage.id, guildId, channel.id, prize, endTime, winnersCount, hoster);
+  } catch (err) {
+      return console.error(`Error in func startGiveaway: ${err}`);
+  };
+};
