90a63dacea926de88c85dd4b6fac18f9d101b5e7
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-05-15 16:23:10 +0530

    Make giveaway auto stop cmd

---
+++ wiki#005/commands/giveawayAutoStop.js
@@ -0,0 +1,27 @@
+module.exports = [{
+    name: "giveaway auto stop",
+    type: "loop",
+    channel: "1123543013470240788",
+    executeOnStartup: true,
+    every: 10 * 1000,
+    code: `
+$djsEval[(async () => {
+const { stopGiveaway } = require(\`\${process.cwd()}/func/giveawayManager.js\`);
+ const { getActiveGiveaways } = require(\`\${process.cwd()}/func/giveawayDB.js\`);
+
+ const g = await getActiveGiveaways();
+ if (!g) return;
+ for (const obj of g) {
+   const t = obj?.endTime;
+   if (!t) continue;
+   if (t && Date.now() >= t) {
+     await stopGiveaway(d, obj?.guildId, obj?.giveawayId , obj?.channelId, "ended"); 
+
+   } else {
+     continue;
+   };
+ };
+})();
+]
+`
+}]
