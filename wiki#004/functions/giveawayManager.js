82724647f260513919f0a46e7eb2d3a867461910
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-05-15 06:44:00 +0530

    Edit: participants and stop giveaway func

--- wiki#004/functions/giveawayManager.js
+++ wiki#004/functions/giveawayManager.js
@@ -65,3 +65,121 @@ async function startGiveaway(prize, duration, interaction, winnersCount, host, c
       return console.error(`Error in func startGiveaway: ${err}`);
   };
 };
+
+/*
+  FUNCTION: participant()
+  USE: To add or remove participant of a giveaway.
+  PARAMS:
+    'interaction': | Type: 'Object' |
+*/
+
+async function participant(interaction) {
+    try {
+      const parts = await getParticipants(interaction.message.id);
+      let m;
+      if (!parts || !parts?.includes(interaction.user.id)) {
+      await addParticipant(interaction.message.id, interaction.user.id);
+      const p = await getParticipants(interaction.message.id);      
+      const a = new ActionRowBuilder()
+      .addComponents(
+      new ButtonBuilder()
+        .setLabel(`🎉 ${p?.length}`)
+        .setCustomId('join-gw')
+        .setStyle(ButtonStyle.Primary)
+      );
+      m = await interaction.channel.messages.fetch(interaction.message.id);
+      await m.edit({ components: [a] });
+      return true;
+    } else {   
+      await removeParticipant(interaction.message.id, interaction.user.id);
+      const p = await getParticipants(interaction.message.id);
+      const a = new ActionRowBuilder()
+      .addComponents(
+      new ButtonBuilder()
+        .setLabel(`🎉 ${p?.length}`)
+        .setCustomId('join-gw')
+        .setStyle(ButtonStyle.Primary)
+      );
+      
+      m = await interaction.channel.messages.fetch(interaction.message.id);
+      await m.edit({ components: [a] });
+    
+     return false;
+    };
+   } catch (err) {
+      return console.error(`Error in func participants: ${err}`);
+  };
+};
+
+/*
+  FUNCTION: stopGiveaway()
+  USE: To stop a giveaway and edit the embed with winners.
+  PARAMS:
+    'client': | Type: 'Object' |
+    'guildId': | Type: 'String' |
+    'giveawayId': | Type: 'String' |
+    'channelId': | Type: 'String' |
+    'status': | Type: 'String' | Choices: ['ended', 'stopped', 'active(not applicable here)', 'cancelled(not applicable here)']
+*/
+
+};
+
+async function stopGiveaway(client, guildId, giveawayId, channelId, status) {
+    try {
+    const dat = await endGiveaway(giveawayId, channelId, status);
+    if (!dat) return false;
+    const guild = await client.guilds.cache.get(guildId); 
+    const n = await guild.channels.cache.get(channelId);
+    if (!n) return false;
+    const p = await getParticipants(giveawayId);
+    const gw = await getGiveaway(giveawayId);
+    const m = await n.messages.fetch(giveawayId);
+    if (!gw || !m) return false;
+    if (gw && m) {
+      const j = chooseWinner(gw?.winnerCount, p);
+    
+      const a = new ActionRowBuilder()
+     	 .addComponents(
+   	 	  new ButtonBuilder()
+     	   .setLabel(`🎉 ${p?.length + (j?.length || 0) || 0}`)
+      	   .setCustomId('join-gw')
+      	   .setDisabled(true)
+       	   .setStyle(ButtonStyle.Primary)
+        );
+      let em; 
+      if (j?.length > 0) {
+        j.forEach((x) => {
+        const user = guild.members.cache.get(x?.replace("<@", '')?.replace('>', '')?.trim());
+        const emb = new EmbedBuilder()
+        		.setTitle("Congratulations!")
+        		.setDescription(`You have won a giveaway!\n\n**Prize:** ${gw?.prize}\n**Guild:** ${guild?.name}`)
+        		.setColor(0xFFFF00)
+        		.setTimestamp();
+        const act = new ActionRowBuilder()
+        		.addComponents(
+                	new ButtonBuilder()
+                    	.setLabel('Giveaway Link')
+                    	.setURL(`https://discord.com/channels/${guildId}/${channelId}/${giveawayId}`)
+                     .setStyle(ButtonStyle.Link)
+                );
+        if (user) user.send({ embeds: [emb], components: [act] }).catch((er) => console.error(er));
+ removeParticipant(giveawayId, x)
+        });
+       m.reply(`Congratulations to winner(s) ${j?.join(", ")}`).catch((err) => console.error(err));
+       em = new EmbedBuilder()
+          .setTitle(`Giveaway Ended!`)
+          .setDescription(`**Giveaway Prize:** ${gw?.prize}\n**Winner(s):** ${j?.join(', ')}`)
+          .setColor(0xFFA500);
+    } else {
+        em = new EmbedBuilder()
+          .setTitle('Giveaway Ended!')
+          .setDescription(`**Giveaway Prize:** ${gw?.prize}\n**Winner(s):** No eligible participants.`)
+          .setColor(0xFFA500);
+    };
+    m.edit({ embeds: [em], components: [a] }).catch((err) => console.error(err));
+    };
+    return true;
+    } catch (err) {
+        return console.error(`Error in func stopGiveaway: ${err}`);
+    };
+};
